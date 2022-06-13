d0e0bd7e91398d5fda65773e8b0b6f54e6b04fbd
GaryCraft <garycraft@our-space.xyz>
2022-06-13 10:18:19 -0500

    Compatible with TypeScript BuildNRun Egg

--- src/events/legacy/ready.ts
+++ src/events/legacy/ready.ts
@@ -4,6 +4,6 @@ import Bot from "../../Bot";
 
 
 export default async (client: Bot) => {
-	console.log(`Ready as ${client.user.username}`);
+	console.log(`Ready as ${client.user.username}`, "App started");
 	client.user.setActivity(`${client.config.activity.message}`, { type: `${client.config.activity.type}` as ExcludeEnum<typeof ActivityTypes, "CUSTOM"> });
 };
