693c67624cbc8b0021ba0cb02d8750bc93c0a450
GaryCraft <garycraft@our-space.xyz>
2022-07-11 17:39:11 -0500

    Add some Commands

---
+++ src/commands/fun/send.ts
@@ -0,0 +1,38 @@
+import { Message, MessageAttachment, TextChannel } from "discord.js";
+import { Permissions } from "discord.js";
+import Bot from "../../Bot";
+
+export default {
+	name: "send",
+	aliases: [],
+	category: "fun",
+	description: "Receive what you sent!",
+	utilization: "{prefix}send",
+	async execute(client: Bot, message: Message, args: string[]): Promise<void> {
+		if (!message.member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && !(client.config.admins.includes(message.member?.id as string))) return;
+
+		let content = args.join(" ");
+		let attachments: MessageAttachment[] | undefined = [];
+		if (message.attachments.size){
+			for await (const [, attachment] of message.attachments) {
+				attachments.push(attachment);
+			}
+		}
+		attachments = attachments.length > 0 ? attachments : undefined;
+		const channelIdRegex = /<#[0-9]{18}>/g;
+		const channelId = args[0];
+		if (channelIdRegex.test(channelId)) {
+			const channel = await client.channels.fetch(channelId.replace(/[<#>]/g, ""));
+			if (!channel) { message.channel.send("Could not find that channel."); return; }
+			if (!(channel instanceof TextChannel)) { message.channel.send("That channel is not a text channel."); return; }
+			content = content.replace(channelIdRegex, "");
+			content = content.trim();
+			content = content.length > 0 ? content : "** **";
+			await channel.send({ content, files: attachments });
+			message.react("✅");
+		} else {
+			content = content.length > 0 ? content : "** **";
+			message.channel.send({ content, files: attachments });
+		}
+	},
+};
