cca2c66000259fb623e81eebd3fc7da01624a8d2
GaryCraft <garycraft@our-space.xyz>
2022-07-26 22:13:34 -0500

    Bring back changes

---
+++ src/interactions/sub_functions/minecraft/minecraft_enabled.ts
@@ -0,0 +1,53 @@
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import Bot from "../../../Bot";
+import GuildData from "../../../database/models/GuildData";
+import MinecraftData from "../../../database/models/MinecraftData";
+import { Interaction } from "../../../types/Executors";
+const interaction: Interaction = {
+	name: "minecraft enabled",
+	type: "SUB_FUNCTION",
+	description: "Configure the Minecraft module enabled.",
+	category: "config",
+	internal_category: "sub",
+	async execute(client: Bot, interaction: CommandInteraction) {
+		const guildDataRepo = client.database.source.getRepository(GuildData);
+		const guildData = await guildDataRepo.findOne({ where: { guildId:`${interaction.guildId}` } });
+		if(!guildData) return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setTitle("Guild Data not initialized")
+					.setDescription("Please initialize this server's data with /config init.")
+					.setColor(`#${client.config.defaultEmbedColor}`)
+			]
+		});
+		const mcDataRepo = client.database.source.getRepository(MinecraftData);
+		const enabled = interaction.options.getBoolean("status") || false;
+		const serverName = interaction.options.getString("server_name");
+		if(enabled && !serverName) return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setTitle("Missing server name")
+					.setDescription("Please provide a server name.")
+					.setColor(`#${client.config.defaultEmbedColor}`)
+			]
+		});
+		const mcData = await mcDataRepo.findOne({ where: { guildId: guildData.guildId } });
+		if (mcData) {
+			await mcDataRepo.save({ ...mcData, enabled: enabled ? enabled : false, serverName: serverName ? serverName : "not_provided" });
+		} else {
+			const guildRepo = client.database.source.getRepository(GuildData);
+			const newMcData = mcDataRepo.create({ guildId: `${guildData.guildId}`, enabled, serverName: serverName ? serverName : "not_provided" });
+			await mcDataRepo.save(newMcData);
+			await guildRepo.save({ ...guildData, verify: newMcData });
+		}
+		await interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setTitle("Minecraft Module Config")
+					.setDescription(`Minecraft Module is now ${enabled ? "**enabled**" : "**disabled**"}`)
+					.setColor(`#${client.config.defaultEmbedColor}`)
+			]
+		});
+	}
+};
+export default interaction;
