73fb50cb33e14f4e64e293bb9a49db86a17832af
GaryCraft <garycraft@our-space.xyz>
2022-09-23 18:04:00 -0500

    Move PaperAPI handlers to a new route

--- src/routes/example.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-import { ServerRequest } from "../types/API";
-export default {
-	async get(_req:ServerRequest, res: Response) {
-		res.redirect("/index.html");
-	},
-};
