c9d923008806779ed963036354c3dca02d13da27
GaryCraft <garycraft@our-space.xyz>
2022-09-25 22:31:31 -0500

    Update Auth Button

--- src/events/buttons/mcAuth.ts
+++ src/events/buttons/mcAuth.ts
@@ -1,39 +1,45 @@
 import { ButtonInteraction, MessageEmbed } from "discord.js";
 import Bot from "../../Bot";
-import MinecraftData from "../../database/models/MinecraftData";
 import MinecraftPlayer from "../../database/models/MinecraftPlayer";
 
 export default async (client: Bot, interaction: ButtonInteraction) => {
 	const id = interaction.customId.split("|")[1];
 	interaction.deferUpdate();
 
-	const mcDataRepo = client.database.source.getRepository(MinecraftData);
 	const mcUserRepo = client.database.source.getRepository(MinecraftPlayer);
-	const mcData = await mcDataRepo.findOne({
+	const mcUser = await mcUserRepo.findOne({
 		where: {
-			guildId: `${interaction.guildId}`
+			uuid: id,
 		},
 	});
-	if (!mcData) return;
-	const users = mcData.players;
-	const user = users.find(p => p.uuid === id);
-	if (!user) return;
+	if (!mcUser) return interaction.editReply({
+		embeds: [
+			new MessageEmbed()
+				.setColor("RED")
+				.setDescription("This user is not registered in the database."),
+		],
+	});
+	const mcData = mcUser.data;
 
+	if (!mcData) return client.logger.error("Minecraft data not found");
+	const players = await mcUserRepo.find();
+	if (!players) return client.logger.error("Minecraft players not found");
+	const player = players.find(p => p.uuid === id);
+	if (!player) return client.logger.warn(`Could not find user with uuid ${id} in database`);
+
+	await mcUserRepo.save({
+		...player,
+		enabled: true
+	});
 	const embed = new MessageEmbed()
 		.setTitle("Authorized!")
 		.setDescription("Your new IP has been authorized.\nYou can now Login")
 		.setColor(`#${client.config.defaultEmbedColor}`);
-
-	await mcUserRepo.save({
-		...user,
-		enabled: true
-	});
-	if(interaction.message.type == "DEFAULT"){
-		await interaction.message.delete().catch(()=>null);
-	}
-	
 	await interaction.channel?.send({
 		embeds: [embed],
 	}).catch(() => null);
-	
+
+	if (interaction.message.type == "DEFAULT") {
+		await interaction.message.delete().catch(() => null);
+	}
 };
