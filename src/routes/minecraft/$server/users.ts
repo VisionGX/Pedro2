00d71731f57d4255c8b217e9dc273a098a5f580d
GaryCraft <garycraft@our-space.xyz>
2022-09-30 14:59:00 -0500

    GET uses path to fulfill server-specified response

--- src/routes/minecraft/$server/users.ts
+++ src/routes/minecraft/$server/users.ts
@@ -9,22 +9,22 @@ export default {
 		const { parentApp: client } = req;
 		
 		if (!req.headers.authorization) {
-			return res.status(403).json({ body: req.body, err: true, code: 403, message: "Invalid authorization!" });
+			return res.status(403).json({ err: true, code: 403, message: "Invalid authorization!" });
 		}
-		if (req.headers.authorization !== `${client.config.api.password}`) return res.status(403).json({ body: req.body, err: true, code: 403, message: "Invalid authorization!" });
+		if (req.headers.authorization !== `${client.config.api.password}`) return res.status(403).json({ err: true, code: 403, message: "Invalid authorization!" });
 
 		const users = await client.database.source.getRepository(MinecraftPlayer).find();
 		const server = await client.database.source.getRepository(MinecraftServer).findOne({
 			where: {
-				identifier: `${req.body.id}`,
+				identifier: `${req.params.server}`,
 			}
 		});
-		if (!server) return res.status(404).json({ body: req.body, err: true, code: 404, message: "Invalid server identifier!" });
+		if (!server) return res.status(404).json({ err: true, code: 404, message: "Invalid server identifier!" });
 		
 		// Match users to server
 		const serverUsers = users.filter(u => u.servers.some(sp => sp.server.identifier === server.identifier));
 		
-		return res.status(200).json({ body: req.body, err: false, code: 200, message: "Success!", data: serverUsers });
+		return res.status(200).json({ err: false, code: 200, message: "Success!", data: serverUsers });
 
 		
 	},
