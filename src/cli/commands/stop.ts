aa0bc6bbb3489fa4cabb71b358bed54d8a18163a
GaryCraft <garycraft@our-space.xyz>
2022-12-14 12:32:05 -0600

    Stop Command can now be delayed

--- src/cli/commands/stop.ts
+++ src/cli/commands/stop.ts
@@ -1,9 +1,13 @@
+import { setTimeout } from "timers/promises";
 import { CliCommand } from "../../types/Executors";
 const cmd: CliCommand = {
 	name: "stop",
 	description: "Stops the bot",
 	usage: "stop",
-	async execute(client, _args) {
+	async execute(client, args) {
+		if(args.length > 0){
+			await setTimeout(parseInt(args[0]) * 1000);
+		}
 		client.logger.info("Stopping Bot...");
 		client.destroy();
 		await client.database.source.destroy();
